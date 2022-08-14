88a5ba0665a9e6395d8fea95de00b58f19c008f8
Decryptus007 <domnick080@outlook.com>
2022-08-14 19:34:46 +0100

    fix changes

--- js/sideBar.js
+++ js/sideBar.js
@@ -1,18 +1,26 @@
 const sideToggle = document.getElementById("sideToggle")
 const sideOutput = document.getElementById("sideOutput")
+const overlay = document.getElementById("overlay")
 
 let sideBarState = false
-sideToggle.addEventListener('click', () => {
+
+function sideBarFunc() {
   if (sideBarState === false) {
+    overlay.classList.remove('hidden')
     sideToggle.classList.remove('fa-bars')
     sideToggle.classList.add('fa-close')
     sideOutput.style.transform = 'translateX(0)'
     sideBarState = true
   } else {
+    overlay.classList.add('hidden')
     sideToggle.classList.remove('fa-close')
     sideToggle.classList.add('fa-bars')
     sideOutput.style.transform = 'translateX(-100%)'
     sideBarState = false
   }
-})
+}
+
+sideToggle.addEventListener('click', () => sideBarFunc())
+
+overlay.addEventListener('click', () => sideBarFunc())
 
